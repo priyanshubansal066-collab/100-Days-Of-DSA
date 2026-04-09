4612bfe08066ed6a55589c4faea88068d24269c4
Priyanshu Bansal <priyanshuagrawal066@gmail.com>
2026-04-09 05:23:39 +0000

    2 new files

---
+++ DAY57Q57.c
@@ -0,0 +1,26 @@
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+void mirror(struct Node* root) {
+    if(root == NULL)
+        return;
+
+    struct Node* temp = root->left;
+    root->left = root->right;
+    root->right = temp;
+
+    mirror(root->left);
+    mirror(root->right);
+}
+
+void inorder(struct Node* root) {
+    if(root == NULL)
+        return;
+
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
