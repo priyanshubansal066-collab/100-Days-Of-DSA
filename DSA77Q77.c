072670787c1e2e115ec06180ba18e6a5f7acf8c6
Priyanshu Bansal <priyanshuagrawal066@gmail.com>
2026-04-18 05:26:58 +0000

    new files 2

--- DSA77Q77.c
+++ DSA77Q77.c
@@ -4,7 +4,7 @@
 
 int visited[MAX];
 
-// DFS function
+// DFS
 void dfs(int node, int adj[MAX][MAX], int n) {
     visited[node] = 1;
 
@@ -22,22 +22,25 @@ int main() {
 
     // Undirected graph
     adj[0][1] = adj[1][0] = 1;
+    adj[1][2] = adj[2][1] = 1;
     adj[2][3] = adj[3][2] = 1;
-
-    int count = 0;
+    adj[3][4] = adj[4][3] = 1;
 
     for(int i = 0; i < n; i++) {
         visited[i] = 0;
     }
 
+    // Start DFS from node 0
+    dfs(0, adj, n);
+
+    // Check all visited
     for(int i = 0; i < n; i++) {
         if(!visited[i]) {
-            dfs(i, adj, n);
-            count++;
+            printf("Graph is NOT connected\n");
+            return 0;
         }
     }
 
-    printf("Number of connected components: %d", count);
-
+    printf("Graph is connected\n");
     return 0;
 }
