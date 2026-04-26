aedf4c62e428696574200514c29a4faf27025b5b
Priyanshu Bansal <priyanshuagrawal066@gmail.com>
2026-04-26 16:13:50 +0000

    new file

---
+++ DAY85Q85.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+void merge(int arr[], int left, int mid, int right) {
+    int i = left, j = mid + 1, k = 0;
+    int temp[100];
+
+    while(i <= mid && j <= right) {
+        if(arr[i] < arr[j])
+            temp[k++] = arr[i++];
+        else
+            temp[k++] = arr[j++];
+    }
+
+    while(i <= mid)
+        temp[k++] = arr[i++];
+
+    while(j <= right)
+        temp[k++] = arr[j++];
+
+    // Copy back
+    for(i = left, k = 0; i <= right; i++, k++) {
+        arr[i] = temp[k];
+    }
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if(left < right) {
+        int mid = (left + right) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+
+int main() {
+    int n, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    mergeSort(arr, 0, n - 1);
+
+    printf("Sorted array:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
